fd22875efbfa68662fd9583802b8bc504b8cd9d9
Deep Goyal <deep.goyal@icloud.com>
2024-06-09 22:33:22 +0530

    highlight selected blocks

--- hatsouka-focus/dist/extension.js
+++ hatsouka-focus/dist/extension.js
@@ -30,27 +30,33 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(__webpack_require__(1));
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "hatsouka-focus" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('hatsouka-focus.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from focus!');
+    let disposable = vscode.commands.registerCommand("hatsouka-focus.helloWorld", () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const selection = editor.selection;
+        const selectedText = editor.document.getText(selection);
+        vscode.window
+            .showInputBox({ prompt: "Add your annotation" })
+            .then((annotation) => {
+            if (annotation) {
+                const decorationType = vscode.window.createTextEditorDecorationType({
+                    backgroundColor: "rgba(255, 255, 0, 0.2)",
+                    border: "1px solid yellow",
+                    isWholeLine: false,
+                });
+                const decoration = { range: selection, hoverMessage: annotation };
+                editor.setDecorations(decorationType, [decoration]);
+            }
+        });
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// This method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 
--- hatsouka-focus/src/extension.ts
+++ hatsouka-focus/src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "hatsouka-focus" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hatsouka-focus" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "hatsouka-focus.helloWorld",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hatsouka-focus.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from focus!');
-	});
+      const selection = editor.selection;
+      const selectedText = editor.document.getText(selection);
 
-	context.subscriptions.push(disposable);
+      vscode.window
+        .showInputBox({ prompt: "Add your annotation" })
+        .then((annotation) => {
+          if (annotation) {
+            const decorationType = vscode.window.createTextEditorDecorationType(
+              {
+                backgroundColor: "rgba(255, 255, 0, 0.2)",
+                border: "1px solid yellow",
+                isWholeLine: false,
+              }
+            );
+
+            const decoration = { range: selection, hoverMessage: annotation };
+            editor.setDecorations(decorationType, [decoration]);
+          }
+        });
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
